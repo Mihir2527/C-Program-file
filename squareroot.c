c69223e6fd0895f77e9f21c1258e6c6bbe99a5c9
Vimlesh Kumar <53998024+Vimlesh-Kumar@users.noreply.github.com>
2019-11-07 15:55:56 +0530

    Create squareroot.c

---
+++ squareroot.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+        float n,m;
+        printf("Enter no for finding root:");
+        scanf("%f",&n);
+        m=sqrt(n);
+        printf("Square root of %f is = %3f\n",n,m);
+        return 0;
+}
